de3bfa62a04e7a0fc67a1f3135ba177fbb2f2bba
Okky Maheswara <okkymahes@gmail.com>
2022-04-03 20:03:58 +0800

    ubah img

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -60,7 +59,7 @@ export default function Home() {
             </div>
             <div className="w-full self-end px-4 lg:w-1/2">
               <div className="relative mt-10 lg:mt-9 lg:right-0">
-                <Image src="/img/sandhika.png" height={500} width={500} alt="Okky Maheswara" className="max-w-full mx-auto" />
+                <img src="/img/sandhika.png" alt="Okky Maheswara" className="max-w-full mx-auto" />
                 <span className="absolute -bottom-0 -z-10 left-1/2 -translate-x-1/2 md:scale-125">
                   <svg width="400" height="400" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                     <path fill="#14B8A6" d="M23.1,-24.1C37.2,-9,61,-4.5,65.6,4.7C70.3,13.9,56,27.7,41.9,28.3C27.7,28.9,13.9,16.2,3.7,12.5C-6.4,8.7,-12.8,14,-26.8,13.4C-40.8,12.8,-62.4,6.4,-69.3,-6.9C-76.1,-20.1,-68.3,-40.3,-54.3,-55.4C-40.3,-70.5,-20.1,-80.5,-7.8,-72.7C4.5,-64.9,9,-39.2,23.1,-24.1Z" transform="translate(100 100) scale(1.1)" />
