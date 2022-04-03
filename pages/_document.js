a29026559c6e07c5d89fd4d7f4f97aee07a63bbd
Okky Maheswara <okkymahes@gmail.com>
2022-04-03 19:44:08 +0800

    ubah dikit

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Mahesora - It's Show Time</title>
+        <meta name="description" content="Portofolio My Sites" />
+        <meta name="keywords" content="HTML, CSS, JavaScript, NextJS, Computer, Portofolio" />
+        <meta name="author" content="Okky Maheswara" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
