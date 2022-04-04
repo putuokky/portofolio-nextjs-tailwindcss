84f55ff1e3d74e3d5234f09403487827c3fc0320
Okky Maheswara <okkymahes@gmail.com>
2022-04-04 15:54:18 +0800

    buat client section

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,22 @@ import { useState } from "react";
 
 export default function Home() {
   const [activeNavbar, setActiveNavbar] = useState(false);
-  onscroll = function () {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-    if (window.pageYOffset > fixedNav) {
-      header.classList.add('navbar-fixed');
-    } else {
-      header.classList.remove('navbar-fixed');
-    }
-  }
+  const [headerOn, setHeaderOn] = useState(false);
+  // window.onscroll = function () {
+  //   const header = document.querySelector('header');
+  //   const fixedNav = header.offsetTop;
+  //   if (window.pageYOffset > fixedNav) {
+  //     header.classList.add('navbar-fixed');
+  //   } else {
+  //     header.classList.remove('navbar-fixed');
+
+  //   }
+  // }
 
   return (
     <>
       {/* Header Start */}
-      <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
+      <header onScroll={() => setHeaderOn()} className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
         <div className="container">
           <div className="flex items-center justify-between relative">
             <div className="px-4">
@@ -37,10 +39,10 @@ export default function Home() {
                     <a href="#about" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Tentang Saya</a>
                   </li>
                   <li className="group">
-                    <a href="#about" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Portfolio</a>
+                    <a href="#portfolio" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Portfolio</a>
                   </li>
                   <li className="group">
-                    <a href="#about" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Clients</a>
+                    <a href="#clients" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Clients</a>
                   </li>
                   <li className="group">
                     <a href="#about" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Blog</a>
@@ -136,6 +138,80 @@ export default function Home() {
         </div>
       </section>
       {/* About Section End */}
+
+      {/* Portfolio Section Start */}
+      <section id="portfolio" className="pt-36 pb-16 bg-slate-100">
+        <div className="container">
+          <div className="w-full px-4">
+            <div className="max-w-lg mx-auto text-center mb-16">
+              <h4 className="font-semibold text-lg text-primary mb-2">Portfolio</h4>
+              <h2 className="font-bold text-dark text-3xl mb-4 sm:text-4xl lg:text-5xl">Project Terbaru</h2>
+              <p className="font-medium text-secondary md:text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo molestiae cumque sequi maiores iure eum, iste voluptas blanditiis repudiandae perferendis!</p>
+            </div>
+          </div>
+          <div className="w-full px-4 flex flex-wrap justify-center lg:w-10/12 lg:mx-auto">
+            <div className="mb-12 p-4 md:w-1/2">
+              <div className="rounded-md shadow-md overflow-hidden">
+                <img src="/img/portfolio/1.png" alt="Portfolio 1" width="w-full" />
+              </div>
+              <h3 className="font-semibold text-xl text-dark mt-5 mb-3">Portfolio Pertama Saya</h3>
+              <p className="font-medium text-base text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nesciunt quisquam distinctio.</p>
+            </div>
+            <div className="mb-12 p-4 md:w-1/2">
+              <div className="rounded-md shadow-md overflow-hidden">
+                <img src="/img/portfolio/2.png" alt="Portfolio 2" width="w-full" />
+              </div>
+              <h3 className="font-semibold text-xl text-dark mt-5 mb-3">Portfolio Kedua Saya</h3>
+              <p className="font-medium text-base text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nesciunt quisquam distinctio.</p>
+            </div>
+            <div className="mb-12 p-4 md:w-1/2">
+              <div className="rounded-md shadow-md overflow-hidden">
+                <img src="/img/portfolio/3.png" alt="Portfolio 2" width="w-full" />
+              </div>
+              <h3 className="font-semibold text-xl text-dark mt-5 mb-3">Portfolio Ketiga Saya</h3>
+              <p className="font-medium text-base text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nesciunt quisquam distinctio.</p>
+            </div>
+            <div className="mb-12 p-4 md:w-1/2">
+              <div className="rounded-md shadow-md overflow-hidden">
+                <img src="/img/portfolio/4.png" alt="Portfolio 4" width="w-full" />
+              </div>
+              <h3 className="font-semibold text-xl text-dark mt-5 mb-3">Portfolio Keempat Saya</h3>
+              <p className="font-medium text-base text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nesciunt quisquam distinctio.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Portfolio Section End */}
+
+      {/* Clients Section Start */}
+      <section id="clients" className="pt-36 pb-32 bg-slate-800">
+        <div className="container">
+          <div className="w-full px-4">
+            <div className="mx-auto text-center mb-16">
+              <h4 className="font-semibold text-lg text-primary mb-2">Clients</h4>
+              <h2 className="font-bold text-white text-3xl mb-4 sm:text-4xl lg:text-5xl">Yang Pernah Bekerjasama</h2>
+              <p className="font-medium text-secondary md:text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, earum. Eum, eius.</p>
+            </div>
+          </div>
+          <div className="w-full px-4">
+            <div className="flex flex-wrap items-center justify-center">
+              <a href="#" className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8">
+                <img src="/img/clients/google.svg" alt="Google" />
+              </a>
+              <a href="#" className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8">
+                <img src="/img/clients/google.svg" alt="Google" />
+              </a>
+              <a href="#" className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8">
+                <img src="/img/clients/google.svg" alt="Google" />
+              </a>
+              <a href="#" className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8">
+                <img src="/img/clients/google.svg" alt="Google" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Clients Section End */}
     </>
   )
 }
