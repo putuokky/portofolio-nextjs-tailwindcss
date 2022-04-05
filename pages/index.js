e587bd657712283e3fd9c4b52adc35f943f283f2
Okky Maheswara <okkymahes@gmail.com>
2022-04-05 20:45:41 +0800

    buat section blog

--- pages/index.js
+++ pages/index.js
@@ -199,19 +199,71 @@ export default function Home() {
                 <img src="/img/clients/google.svg" alt="Google" />
               </a>
               <a href="#" className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8">
-                <img src="/img/clients/google.svg" alt="Google" />
+                <img src="/img/clients/gojek.svg" alt="Gojek" />
               </a>
               <a href="#" className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8">
-                <img src="/img/clients/google.svg" alt="Google" />
+                <img src="/img/clients/tokopedia.svg" alt="Tokopedia" />
               </a>
               <a href="#" className="max-w-[120px] mx-4 py-4 grayscale opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100 lg:mx-6 xl:mx-8">
-                <img src="/img/clients/google.svg" alt="Google" />
+                <img src="/img/clients/traveloka.svg" alt="Traveloka" />
               </a>
             </div>
           </div>
         </div>
       </section>
       {/* Clients Section End */}
+
+      {/* Blog Section Start */}
+      <section id="blog" className="pt-36 pb-32 bg-slate-100">
+        <div className="container">
+          <div className="w-full px-4">
+            <div className="max-w-lg mx-auto text-center mb-16">
+              <h4 className="font-semibold text-lg text-primary mb-2">Blog</h4>
+              <h2 className="font-bold text-dark text-3xl mb-4 sm:text-4xl lg:text-5xl">Tulisan Terkini</h2>
+              <p className="font-medium text-secondary md:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quas ipsum dicta porro.</p>
+            </div>
+          </div>
+          <div className="flex flex-wrap">
+            <div className="w-full px-4 lg:w-1/2 xl:w-1/3">
+              <div className="bg-white rounded-xl shadow-lg overflow-hidden mb-10">
+                <img src="https://source.unsplash.com/360x200?programming" alt="Programming" className="w-full" />
+                <div className="py-8 px-6">
+                  <h3>
+                    <a href="#" className="block mb-3 font-semibold text-xl text-dark hover:text-primary truncate">Tips Belajar Programming</a>
+                  </h3>
+                  <p className="font-medium text-base text-secondary mb-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, similique.</p>
+                  <a href="#" className="font-medium text-sm text-white bg-primary py-3 px-4 rounded-lg hover:opacity-80">Baca Selengkapnya</a>
+                </div>
+              </div>
+            </div>
+            <div className="w-full px-4 lg:w-1/2 xl:w-1/3">
+              <div className="bg-white rounded-xl shadow-lg overflow-hidden mb-10">
+                <img src="https://source.unsplash.com/360x200?mechanical+keyboard" alt="Mechanical Keyboard" className="w-full" />
+                <div className="py-8 px-6">
+                  <h3>
+                    <a href="#" className="block mb-3 font-semibold text-xl text-dark hover:text-primary truncate">Review Keyboad Mechanical Terbaru</a>
+                  </h3>
+                  <p className="font-medium text-base text-secondary mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et molestiae rerum repellat?</p>
+                  <a href="#" className="font-medium text-sm text-white bg-primary py-3 px-4 rounded-lg hover:opacity-80">Baca Selengkapnya</a>
+                </div>
+              </div>
+            </div>
+            <div className="w-full px-4 lg:w-1/2 xl:w-1/3">
+              <div className="bg-white rounded-xl shadow-lg overflow-hidden mb-10">
+                <img src="https://source.unsplash.com/360x200?coffe" alt="Coffe" className="w-full" />
+                <div className="py-8 px-6">
+                  <h3>
+                    <a href="#" className="block mb-3 font-semibold text-xl text-dark hover:text-primary truncate">Menikmati Coffe Disaat Ngoding</a>
+                  </h3>
+                  <p className="font-medium text-base text-secondary mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores veniam repellendus eum.</p>
+                  <a href="#" className="font-medium text-sm text-white bg-primary py-3 px-4 rounded-lg hover:opacity-80">Baca Selengkapnya</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Blog Section End */}
     </>
   )
 }
