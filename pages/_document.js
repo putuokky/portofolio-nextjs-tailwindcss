e49f6745c13b707c7e1aee23d79d3df0b49857a6
Okky Maheswara <okkymahes@gmail.com>
2022-05-21 21:34:28 +0800

    ubah meta revisii

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,11 @@ export default function Document() {
     <Html className='scroll-smooth'>
       <Head>
         <title>Mahesora - It's Show Time</title>
+
         <meta name="description" content="Portofolio My Sites" />
         <meta name="keywords" content="HTML, CSS, JavaScript, NextJS, Computer, Portofolio" />
         <meta name="author" content="Okky Maheswara" />
-        <meta property="og:title" content="Mahesora - It's Show Time" key="title" />
+
         {/* <link rel="icon" type="image/x-icon" href="/favicon.ico"></link> */}
       </Head>
       <body>
