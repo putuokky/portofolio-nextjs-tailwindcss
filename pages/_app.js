66d04816e26bbb66635c917033b774fcdb2943e0
Okky Maheswara <okkymahes@gmail.com>
2022-05-24 20:29:51 +0800

    buat dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Mahesora &mdash; It's Show Time</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html className='scroll-smooth'>
+    <Html className='scroll-smooth dark'>
       <Head>
-        <title>Mahesora &mdash; It's Show Time</title>
         <meta name="description" content="Portofolio My Sites" />
         <meta name="keywords" content="HTML, CSS, JavaScript, NextJS, Computer, Portofolio" />
         <meta name="author" content="Okky Maheswara" />
